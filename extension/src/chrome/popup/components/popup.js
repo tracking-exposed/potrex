aa1197dbd42d282283e8176003733fbc36e475a4
Claudio <claudio@tracking.exposed>
2019-12-23 19:59:47 +0100

    completed popup new gfx

--- extension/src/chrome/popup/components/popup.js
+++ extension/src/chrome/popup/components/popup.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import InfoBox from './infoBox';
+import config from '../../../config';
 
 const Popup = React.createClass({
   render () {
+
+    const personalLink = config.WEB_ROOT + '/personal/#' + this.props.publicKey;
+
     return (
         <div className="containerstyle">
-          <div className="headerstyle">
+          <div className="headerstyle colors-bg">
             <a href="https://pornhub.tracking.exposed" target="_blank">
               <img className="svglimit" src="/header-logo-pornhub.svg" alt="logo" />
             </a>
@@ -15,7 +18,11 @@ const Popup = React.createClass({
               This is <a target="_blank" href='https://tracking.exposed'>Tracking Exposed</a>. Thank you for believing in our dirtiest side project ;)
             </p>
           </div>
-          <InfoBox {...this.props} />
+          <div className="headerstyle white-bg">
+            <a target='_blank' href={personalLink}>
+              <img className="svglimit" src='/accessyourdata.svg' />
+            </a>
+          </div>
           <div className="textstyle">
             <p>Running on
               <span> </span>
