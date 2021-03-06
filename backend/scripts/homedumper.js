b8df7cf4f41f802a34007452bc84d8bece63c60a
Claudio <claudio@syrianarchive.org>
2021-03-06 20:22:16 +0100

    added filter for RFY cat

--- backend/scripts/homedumper.js
+++ backend/scripts/homedumper.js
@@ -211,8 +211,10 @@ function shrinkData(bigen) {
     return r;
 }
 
-function produceDot(data, filename) {
-    
+function produceDot(allcatdata, filename) {
+   
+    // we only keep this category for this test
+    const data = _.filter(allcatdata, { sectionName: 'Recommended For You'}),
     const dot = Object({links: [], nodes: []})
     dot.links = _.map(data, function(video) { return { target: video.who, source: video.videoId, value: 1} });
 
